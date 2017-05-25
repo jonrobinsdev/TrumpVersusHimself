3cebeab5f453c2d845f63ddaa849ac06af4e1e8c
Jonathan Robins <jonrobns@gmail.com>
2017-05-24 22:15:14 -0400

    MORE. CONTENT.

---
+++ js/templates.js
@@ -0,0 +1,11 @@
+"<div class=\"date-container\"><p class=\"date\">" + articleDate + "</p></div><div class=\"main-compare\"><!--<div class=\"date\"></div>--><div class=\"main-compare-row row small-collapse medium-uncollapse\"><div class=\"cheeto-tweet new small-12 medium-6 large-6 columns\"><div class=\"cheeto-tweet-container\">" + tweetHTML + "</div></div><div class=\"news-article new small-12 medium-6 large-6 columns\"><div class=\"news-article-container\"><a href=\"" + articleUrl + "\" target=\"_blank\"><img class=\"news-article-img\" src=\"" + articleImageUrl + "\"/></a><div class=\"news-article-title-link-container\"><h4 class=\"news-article-title\"><a class=\"news-article-title-link\" href=\"" + articleUrl + "\" target=\"_blank\">" + articleTitle + "</a></h4><a class=\"news-article-link\" href=\"" + articleUrl + "\" target=\"_blank\">" + articleSubtitle + "</a></div></div></div></div></div>"
+
+{
+	"articleNo":0,
+	"articleDate":"05/05/2017",
+	"articleUrl":"", 
+	"articleImageUrl":"i/news-article-thumbnails/0.png", 
+	"articleTitle":"",
+	"articleSubtitle":"Read the article on cnn.com >",
+	"tweetHTML":""
+},
