6e616ad1b1eb2732a3615936cc7c8261e30c482b
Jonathan Robins <jonrobns@gmail.com>
2017-05-26 21:46:54 -0400

    permalinks

--- js/helper.js
+++ js/helper.js
@@ -71,13 +71,14 @@ function slideInRight(){
 }
 
 function createNewCompareElement(index){
+	var articleNumber = index;
 	var articleDate = this.compareData[index].articleDate;
 	var articleUrl = this.compareData[index].articleUrl;
 	var articleImageUrl = this.compareData[index].articleImageUrl;
 	var articleTitle = this.compareData[index].articleTitle;
 	var articleSubtitle = this.compareData[index].articleSubtitle;
 	var tweetHTML = this.compareData[index].tweetHTML;
-	var newCompareElement = "<div class=\"date-container\"><p class=\"date\">" + articleDate + "</p></div><div class=\"main-compare\"><!--<div class=\"date\"></div>--><div class=\"main-compare-row row small-collapse medium-uncollapse\"><div class=\"cheeto-tweet new small-12 medium-6 large-6 columns\"><div class=\"cheeto-tweet-container\">" + tweetHTML + "</div></div><div class=\"news-article new small-12 medium-6 large-6 columns\"><div class=\"news-article-container\"><a href=\"" + articleUrl + "\" target=\"_blank\"><img class=\"news-article-img\" src=\"" + articleImageUrl + "\"/></a><div class=\"news-article-title-link-container\"><h4 class=\"news-article-title\"><a class=\"news-article-title-link\" href=\"" + articleUrl + "\" target=\"_blank\">" + articleTitle + "</a></h4><a class=\"news-article-link\" href=\"" + articleUrl + "\" target=\"_blank\">" + articleSubtitle + "</a></div></div></div></div></div>";
+	var newCompareElement = "<div class=\"date-container\"><p class=\"date\">" + articleDate + "</p></div><div class=\"main-compare\"><div class=\"main-compare-row row small-collapse medium-uncollapse\"><div class=\"cheeto-tweet new small-12 medium-6 large-6 columns\"><div class=\"cheeto-tweet-container\">" + tweetHTML + "</div></div><div class=\"news-article new small-12 medium-6 large-6 columns\"><div class=\"news-article-container\"><a href=\"" + articleUrl + "\" target=\"_blank\"><img class=\"news-article-img\" src=\"" + articleImageUrl + "\"/></a><div class=\"news-article-title-link-container\"><h4 class=\"news-article-title\"><a class=\"news-article-title-link\" href=\"" + articleUrl + "\" target=\"_blank\">" + articleTitle + "</a></h4><a class=\"news-article-link\" href=\"" + articleUrl + "\" target=\"_blank\">" + articleSubtitle + "</a></div></div></div></div></div><div class=\"permalink-container\"><a class=\"permalink\" href=\"http://trumpversushimself.com/article/?articleNumber=" + articleNumber + "\">Permalink</a></div>";
 	if (!newCompareElement.includes("<script>") && !newCompareElement.includes("<iframe>") && !newCompareElement.includes("onerror")) {
 		return newCompareElement;
 	} else {
